63403859259f1f2c701c30c02539bfe00d70406a
CandyPanda-LS <it19132310@my.sliit.lk>
2021-05-07 20:58:09 +0530

    File structure

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                Gmmak thami bokka
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,2 @@
+var e,r=require("react-dom").render,t=require("react"),n=(e=t)&&e.__esModule?e.default:e,a=t.Component;class l extends a{constructor(e){super(e)}render(){return n.createElement("div",null,"Gmmak thami bokka")}}r(n.createElement(l,null),document.getElementById("app"));
+//# sourceMappingURL=index.js.map
