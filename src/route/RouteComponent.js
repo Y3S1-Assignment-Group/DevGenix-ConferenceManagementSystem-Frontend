0e5d4d135ae2449b59f29091b92645b69d0ad995
CandyPanda-LS <it19132310@my.sliit.lk>
2021-05-18 00:04:09 +0530

    Wershop Presenter reg UI

--- src/route/RouteComponent.js
+++ src/route/RouteComponent.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage";
 import RegPresenter from "../pages/RegPresenter/RegPresenter";
 import RegAttendee from "../pages/RegAttendee/RegAttendee";
+import RegRearseacher from "../pages/RegReasearcher/RegRearseacher";
 
 class RouteComponent extends Component {
   render() {
@@ -11,6 +12,7 @@ class RouteComponent extends Component {
         <Switch>
           <Route path="/" exact component={HomePage} />
             <Route path="/PresenterRegistration" exact component={RegPresenter} />
+            <Route path="/ReasearcherRegistration" exact component={RegRearseacher} />
             <Route path="/AttendeeRegistration" exact component={RegAttendee} />
 
 
