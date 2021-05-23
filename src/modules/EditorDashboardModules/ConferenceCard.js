ab6666cfa29456cc4c9daff1d3dee1eb8f006794
SenuraJayadeva <senurajayadeva@gmail.com>
2021-05-23 17:25:22 +0530

    Get Conference Details in Editor Dashboard

--- src/modules/EditorDashboardModules/ConferenceCard.js
+++ src/modules/EditorDashboardModules/ConferenceCard.js
@@ -1,46 +1,106 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../../../actions/conferenceActions";
 import { Button, Card, CardBody, CardText, CardTitle } from "reactstrap";
 import { FaRegFilePowerpoint } from "react-icons/fa";
 import { BsCalendar } from "react-icons/bs";
 import { CgProfile } from "react-icons/cg";
 
 export class ConferenceCard extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchAllApprovedConferenceList();
+  }
+
   render() {
     return (
       <div>
-        <Card
-          style={{
-            borderRadius: "20px",
-            backgroundColor: "#21012B",
-            marginLeft: "20px",
-          }}
-        >
-          <CardBody>
-            <CardTitle tag="h5" style={{ color: "white" }}>
-              Introduction to flutter
-            </CardTitle>
-            <CardText style={{ color: "white" }}>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </CardText>
+        {this.props.approvedConferenceList.map((singleConference) => {
+          return (
+            <Card
+              style={{
+                borderRadius: "20px",
+                backgroundColor: "#21012B",
+                marginLeft: "20px",
+              }}
+              className="text-center"
+            >
+              <CardBody>
+                <CardTitle style={{ color: "white" }}>
+                  <div className="row">
+                    <div className="col-md-6 text-center">Title </div>
 
-            <Button className="bg-transparent border-0">
-              <FaRegFilePowerpoint color="white" fontSize="1.75em" />
-            </Button>
+                    <div className="col-md-6 text-left">
+                      {singleConference.confTitle}
+                    </div>
+                  </div>
+                </CardTitle>
+                <center>
+                  {" "}
+                  <CardText style={{ color: "white" }}>
+                    <div className="row">
+                      <div className="col-md-6 text-center">About </div>
 
-            <Button className="bg-transparent border-0">
-              <BsCalendar color="white" fontSize="1.5em" />
-            </Button>
+                      <div className="col-md-6 text-left">
+                        {" "}
+                        {singleConference.description}
+                      </div>
+                    </div>
+                  </CardText>
+                  <CardText style={{ color: "white" }}>
+                    <div className="row">
+                      <div className="col-md-6 text-center">From</div>
 
-            <Button className="bg-transparent border-0">
-              <CgProfile color="white" fontSize="1.7em" />
-            </Button>
-          </CardBody>
-        </Card>
+                      <div className="col-md-6 text-left">
+                        {" "}
+                        {singleConference.fromTime}
+                      </div>
+                    </div>
+                  </CardText>
+                  <CardText style={{ color: "white" }}>
+                    <div className="row">
+                      <div className="col-md-6 text-center">To</div>
+
+                      <div className="col-md-6 text-left">
+                        {" "}
+                        {singleConference.toTime}
+                      </div>
+                    </div>
+                  </CardText>
+                  <CardText style={{ color: "white" }}>
+                    <div className="row">
+                      <div className="col-md-6 text-center">Venue</div>
+
+                      <div className="col-md-6 text-left">
+                        {" "}
+                        {singleConference.venue}
+                      </div>
+                    </div>
+                  </CardText>
+                  <Button className="bg-transparent border-0">
+                    <FaRegFilePowerpoint color="white" fontSize="1.75em" />
+                  </Button>
+                  <Button className="bg-transparent border-0">
+                    <BsCalendar color="white" fontSize="1.5em" />
+                  </Button>
+                </center>
+              </CardBody>
+            </Card>
+          );
+        })}
       </div>
     );
   }
 }
 
-export default ConferenceCard;
+const mapStateToProps = (state) => ({
+  approvedConferenceList: state.conferenceReducer.approvedConferenceList,
+});
+
+const mapActionToProps = {
+  fetchAllApprovedConferenceList: actions.fetchAllApprovedConferenceList,
+};
+export default connect(mapStateToProps, mapActionToProps)(ConferenceCard);
