6636aafebc05446b03c4bd3c95d3c7002cca92d2
Dilmi101 <dnimasha971@gmail.com>
2021-05-23 23:54:46 +0530

    add template

--- src/common/StripePayment/StripeCheckout.js
+++ src/common/StripePayment/StripeCheckout.js
@@ -56,6 +56,7 @@ class StripeCheckout extends Component {
                     registerAttendeeObj,
                     () => {
                         alert("Success");
+                        window.location = "/";
                     },
                     () => {
 
--- src/modules/EditorDashboardModules/AddTemplates.js
+++ src/modules/EditorDashboardModules/AddTemplates.js
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../../../actions/templateActions";
+import { storage } from "../../firebase";
+import Progress from "../../common/ProgressBar/Progress";
+
 import {
   Card,
   CardBody,
@@ -11,6 +16,111 @@ import {
 } from "reactstrap";
 
 export class AddTemplates extends Component {
+  constructor(props) {
+    super(props);
+    this.onAddTemplate = this.onAddTemplate.bind(this);
+    this.uploadFile = this.uploadFile.bind(this);
+    this.state = {
+      templateName: "",
+      fileLink: "",
+      processStatus: false,
+      processStatusAlert: "",
+      processStatusMessage: "",
+     
+      fileProcessStatus: false,
+      fileProcessStatusAlert: "",
+      fileProcessStatusMessage: "",
+
+      uploadProfilePercentage: 0,
+      uploadTemplateImagePercentage: 0,
+      uploadTemplateFilePercentage: 0,
+    }
+  }
+
+  onValueChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  uploadFile(e) {
+    if (e.target.files[0] !== null) {
+      this.setState({
+        fileProcessStatus: true,
+        fileProcessStatusAlert: "alert alert-warning",
+        fileProcessStatusMessage: "File Uploading...",
+      });
+      const uploadTask = storage
+        .ref(`templatefiles/${e.target.files[0].name}`)
+        .put(e.target.files[0]);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          //progress function
+          const progress = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          this.setState({ uploadTemplateFilePercentage: progress });
+        },
+        (error) => {
+          //error function
+          console.log(error);
+        },
+        () => {
+          //complete function
+          storage
+            .ref("templatefiles")
+            .child(e.target.files[0].name)
+            .getDownloadURL()
+            .then((url) => {
+              console.log(url);
+              this.setState({ fileLink: url });
+              this.setState({
+                fileProcessStatusAlert: "alert alert-success",
+                fileProcessStatusMessage: "File uploaded successfully",
+              });
+            });
+        }
+      );
+    } else {
+      this.setState({
+        fileProcessStatusAlert: "alert alert-danger",
+        fileProcessStatusMessage: "Something went wrong",
+      });
+    }
+  }
+
+  onAddTemplate(e) {
+    e.preventDefault();
+
+    this.setState({
+      processStatus: false,
+      processStatusAlert: "",
+      processStatusMessage: "",
+    })
+
+    const addTemplates = {
+      templateName: this.state.templateName,
+      fileLink: this.state.fileLink,
+    };
+
+    this.props.addTemplate(
+      addTemplates,
+      () => {
+        this.setState({
+          processStatusAlert: "alert alert-success",
+          processStatusMessage: "Successful",
+        });
+        window.location = "/editordashboard";
+      },
+      () => {
+        this.setState({
+          processStatusAlert: "alert alert-danger",
+          processStatusMessage:
+            "Please try again.",
+        });
+      }
+    )
+  }
+
   render() {
     return (
       <div>
@@ -19,13 +129,15 @@ export class AddTemplates extends Component {
             <CardBody className="p-5">
               <h3 className="text-center">ADD | TEMPLATE</h3>
               <hr />
-              <Form>
+              <Form onSubmit={this.onAddTemplate}>
                 <FormGroup row>
                   <Label for="WordLink" sm={4}>
                     Template Name
                   </Label>
                   <Col sm={8}>
-                    <Input type="text" name="templateName" id="templateName" />
+                    <Input type="text" name="templateName" id="templateName"  onChange={(e) => {
+                            this.onValueChange(e);
+                          }}/>
                   </Col>
                 </FormGroup>
                 <FormGroup row>
@@ -33,8 +145,26 @@ export class AddTemplates extends Component {
                     File
                   </Label>
                   <Col sm={8}>
-                    <Input type="file" name="WordLink" id="WordLink" />
+                    <Input type="file" name="fileLink" id="fileLink"  onChange={(e) => {this.uploadFile(e);}}/>
                   </Col>
+                </FormGroup>
+                
+                <FormGroup>
+                  {this.state.fileProcessStatus ? (
+                    <div
+                      className={this.state.fileProcessStatusAlert}
+                      role="alert"
+                    >
+                      {this.state.fileProcessStatusMessage}
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                </FormGroup>
+                <FormGroup row>
+                  <Progress
+                    percentage={this.state.uploadTemplateFilePercentage}
+                  />
                 </FormGroup>
 
                 <FormGroup check row>
@@ -53,4 +183,8 @@ export class AddTemplates extends Component {
   }
 }
 
-export default AddTemplates;
+const mapActionToProps = {
+  addTemplate: actions.addTemplate,
+};
+
+export default connect(null, mapActionToProps)(AddTemplates);
