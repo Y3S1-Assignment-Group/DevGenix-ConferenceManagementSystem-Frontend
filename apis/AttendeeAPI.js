4d9bc5ba794f20d94ad3de0ba78719d19f079ed1
SenuraJayadeva <senurajayadeva@gmail.com>
2021-05-23 22:24:50 +0530

    Atendee Table Completed

--- apis/AttendeeAPI.js
+++ apis/AttendeeAPI.js
@@ -1,15 +1,23 @@
 import axios from "axios";
+import authHeader from "./authHeader";
 
 const baseUrl = process.env.REACT_APP_BACKEND_URL;
 
+const config = {
+  headers: authHeader(),
+};
+
 const attendeeApi = {
-    auth() {
-        return {
-            register: (newAttendee) =>
-            axios.post(baseUrl + "/api/authAttendee/register", newAttendee),
-            login: (loginAttendee) => axios.post(baseUrl + "/api/authAttendee", loginAttendee)
-        };    
-    },
+  auth() {
+    return {
+      register: (newAttendee) =>
+        axios.post(baseUrl + "/api/authAttendee/register", newAttendee),
+      login: (loginAttendee) =>
+        axios.post(baseUrl + "/api/authAttendee", loginAttendee),
+      getAllAttendees: () =>
+        axios.get(baseUrl + "/api/authAttendee/all", config),
+    };
+  },
 };
 
 export default attendeeApi;
